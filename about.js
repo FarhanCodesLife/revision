f9d5c1e59c889cb9114f42cf678c05db9564c55e
MUHAMMADFARHANHANIF <farhansmit0318@gamil.com>
2024-06-24 01:57:52 -0700

    js es6 revision

---
+++ about.js
@@ -0,0 +1,12 @@
+import { username } from "./app.js";
+import { age } from "./app.js";
+import { email } from "./app.js";
+
+// import { username,age,email } from "./app.js";
+
+
+// import Number from "./app.js";  import defalut 
+console.log(username);
+console.log(email("farhan@gmail.com"));
+console.log(age(19));
+console.log(Number);
---
+++ app.js
@@ -0,0 +1,168 @@
+
+// const div = document.querySelector('div')
+
+// async function products() {
+
+//     try {
+//         let data = await axios('http://api.weatherapi.com/v1/current.json?key=6326e206cf9041e5b3853528241406&q=London&aqi=no')
+//     data.data.map((item)=>{
+
+//         div.innerHTML +=`
+//     <h1>${data.data.location.country}</h1>
+//     <h1>${data.data.location.name}</h1>
+//     <h1>${data.data.location.localtime}</h1>
+
+//     `
+//     })
+    
+    
+    
+    
+    
+//     console.log(data.data);
+
+//     } catch (error) {
+//         console.log(error);
+
+//     }
+
+// }
+
+// products()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// spread opretars
+
+
+
+// const array1 =['a','b','c']
+// const array2 = [1 ,2 ,3 ]
+
+
+// const  arraysum = [...array1 ,...array2]
+// console.log(arraysum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// rest operaters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Module
+
+
+
+
+export let username = "MuhammadFarhan"
+
+export function age(age) {
+    return age
+}
+
+
+export function email(email) {
+    return email
+    
+}
+
+
+// export{username , age ,email}
+
+
+
+let phoneNumber = 3182127256
+export default phoneNumber
+
+
+
+
+
+
+
+
+
+
+
+
+
+
